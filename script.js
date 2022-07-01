76b1907644dec3cbdc4cbffd827620cd790a3d27
Rizky Hafizh Jatmiko <rizky0409j@gmail.com>
2022-07-01 17:04:19 +0700

    Page Article Responsive (except Futer)

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let menu = document.getElementById("menu").addEventListener("click", btn_menu);
+let changeIcon = function (icon) {
+  icon.classList.toggle("fa-times");
+};
 
-function btn_menu() {
-  console.log("halo");
+function toggleNav() {
+  let burger = document.getElementById("navbar");
+  if (burger.style.opacity == 0) {
+    burger.style.opacity = 1;
+  } else {
+    burger.style.opacity = 0;
+  }
 }
+
+// onclick = "changeIcon(this)";
